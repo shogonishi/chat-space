331effe851e530da1401b07d4cd63ac2effcb3a7
shogonishi <themasterplan1986@gmail.com>
2018-10-18 15:50:23 +0900

    インクリメンタルサーチ機能
    # WHAT
    ユーザーの検索機能
    # WHY
    チャットグループ作成のため

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resource :user, only: [:edit, :update]
+  resources :users, only: [:index]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
